f1bde9e1502271b5ec22e6ef49ef49cb81f8a1c7
Kartik0806 <93075526+Kartik0806@users.noreply.github.com>
2025-04-16 19:12:20 +0530

    Update Meet.jsx

--- frontend/src/pages/Meet.jsx
+++ frontend/src/pages/Meet.jsx
@@ -5,6 +5,9 @@ import { useAuth } from "../providers/authProvider.jsx";
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/';
 
+const SOCKET_URL = import.meta.env.PROD
+  ? "https://peer-to-peer-bun5.onrender.com"
+  : "http://localhost:5000";
 
 export const Meet = () => {
     const { id } = useParams();
@@ -68,7 +71,7 @@ export const Meet = () => {
             }
         };
 
-        const newSocket = io(API_URL, {
+        const newSocket = io(SOCKET_URL, {
             withCredentials: true,
             query: { meetCode },
             auth: { user_token: user?.token || "" },  // Use actual user token
